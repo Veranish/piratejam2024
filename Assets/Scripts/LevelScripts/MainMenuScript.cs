9b7356f6231bdb4f775cacabb0c7be203660bd87
JudgeRW <robwampner@gmail.com>
2024-07-24 17:44:02 -0500

    Implements first simple menu

---
+++ Assets/Scripts/LevelScripts/MainMenuScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void Play()
+    {
+        // if player hits the play button, swap to the next scene (iterate from 0 - menu to 1 - first game level)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        // if player hits the quit button, exit the game
+        Application.Quit();
+    }
+}
